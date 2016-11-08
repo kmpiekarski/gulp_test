e40ea381506e6e14470c00a06c8f65281407abae
slashedtires <kenneth.m.piekarski@gmail.com>
2016-11-08 14:13:58 -0800

    setup SASS preprocessor test

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,12 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 
 gulp.task('sup', function() {
  console.log('\'sup');
+});
+
+gulp.task('sass',function(){
+  return gulp.src('app/scss/main.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('app/css'))
 });
